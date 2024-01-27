669ca77ad15e6f03c70c2669eab7669240bdc336
yongj.zhuang <yongj.zhuang@outlook.com>
2024-01-27 23:06:52 +0800

    Use TTLCache to avoid compressing same images repeatedly on service unavailable

--- internal/hammer/server.go
+++ internal/hammer/server.go
@@ -4,10 +4,15 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/curtisnewbie/hammer/api"
 	fstore "github.com/curtisnewbie/mini-fstore/client"
 	"github.com/curtisnewbie/miso/miso"
+)
+
+var (
+	compressedImageFileIdCache = miso.NewTTLCache[string](15*time.Minute, 200)
 )
 
 func BootstrapServer(args []string) {
@@ -22,20 +27,49 @@ func BootstrapServer(args []string) {
 func ListenCompressImageEvent(rail miso.Rail, evt api.ImageCompressTriggerEvent) error {
 	rail.Infof("Received CompressImageEvent: %+v", evt)
 
+	// wrap the compression logic with cache
+	// it's possible that we compressed the image but somehow failed to reply to rabbitmq
+	// using a tiny time-based cache can avoid compressing the same images again and again
+	var err error
+	generatedFileId, ok := compressedImageFileIdCache.Get(evt.Identifier, func() (string, bool) {
+		generatedFileId, er := CompressImage(rail, evt)
+		err = er
+		return generatedFileId, er == nil && generatedFileId != ""
+	})
+	if err != nil {
+		return err
+	}
+	if !ok {
+		return nil
+	}
+
+	// reply to the specified event bus
+	err = miso.PubEventBus(rail,
+		api.ImageCompressReplyEvent{Identifier: evt.Identifier, FileId: generatedFileId},
+		evt.ReplyTo)
+	if err == nil {
+		compressedImageFileIdCache.Del(evt.Identifier)
+	}
+	return err
+}
+
+func CompressImage(rail miso.Rail, evt api.ImageCompressTriggerEvent) (string, error) {
+	rail.Infof("Received CompressImageEvent: %+v", evt)
+
 	originFile, err := fstore.FetchFileInfo(rail, fstore.FetchFileInfoReq{FileId: evt.FileId})
 	if err != nil {
 		if errors.Is(err, fstore.ErrFileDeleted) || errors.Is(err, fstore.ErrFileNotFound) {
 			rail.Warnf("File %v is not found or deleted, %v", evt.FileId, evt.Identifier)
-			return nil
+			return "", nil
 		}
-		return fmt.Errorf("failed to fetch fstore file info: %v, %v", evt.FileId, err)
+		return "", fmt.Errorf("failed to fetch fstore file info: %v, %v", evt.FileId, err)
 	}
 
 	// generate temp token for downloading file from mini-fstore
 	originFileToken, e := fstore.GenTempFileKey(rail, evt.FileId, "")
 	if e != nil {
 		rail.Errorf("Failed to GetFstoreTmpToken, %v", e)
-		return nil
+		return "", nil
 	}
 	rail.Infof("tkn: %v", originFileToken)
 
@@ -43,13 +77,13 @@ func ListenCompressImageEvent(rail miso.Rail, evt api.ImageCompressTriggerEvent)
 	downloadPath := "/tmp/" + miso.RandNum(20)
 	downloadFile, err := os.Create(downloadPath)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer downloadFile.Close()
 
 	if e := fstore.DownloadFile(rail, originFileToken, downloadFile); e != nil {
 		rail.Errorf("Failed to DownloadFstoreFile, %v", e)
-		return nil
+		return "", nil
 	}
 	rail.Infof("File downloaded to %v", downloadPath)
 	defer os.Remove(downloadPath)
@@ -58,7 +92,7 @@ func ListenCompressImageEvent(rail miso.Rail, evt api.ImageCompressTriggerEvent)
 	compressPath := downloadPath + "_compressed"
 	if e := GiftCompressImage(rail, downloadPath, compressPath); e != nil {
 		rail.Errorf("Failed to compress image, giving up, %v", e)
-		return nil // don't retry
+		return "", nil // don't retry
 	}
 	defer os.Remove(compressPath)
 	rail.Infof("Image %v compressed to %v", evt.Identifier, compressPath)
@@ -66,25 +100,22 @@ func ListenCompressImageEvent(rail miso.Rail, evt api.ImageCompressTriggerEvent)
 	// upload the compressed image to mini-fstore
 	compressedFile, err := os.Open(compressPath)
 	if err != nil {
-		return fmt.Errorf("failed to open compressed file, file: %v, %v", compressPath, err)
+		return "", fmt.Errorf("failed to open compressed file, file: %v, %v", compressPath, err)
 	}
 	defer compressedFile.Close()
 
 	uploadFileId, e := fstore.UploadFile(rail, originFile.Name+"_thumbnail", compressedFile)
 	if e != nil {
 		rail.Errorf("Failed to UploadFstoreFile, %v", e)
-		return fmt.Errorf("failed to upload fstore file, %v", e)
+		return "", fmt.Errorf("failed to upload fstore file, %v", e)
 	}
 
 	// exchange the uploadFileId with the real fileId
 	thumbnailFile, e := fstore.FetchFileInfo(rail, fstore.FetchFileInfoReq{UploadFileId: uploadFileId})
 	if e != nil {
 		rail.Errorf("Failed to FetchFstoreFileInfo, %v", e)
-		return fmt.Errorf("failed to fetch fstore file info, %v", e)
+		return "", fmt.Errorf("failed to fetch fstore file info, %v", e)
 	}
 
-	// reply to the specified event bus
-	return miso.PubEventBus(rail,
-		api.ImageCompressReplyEvent{Identifier: evt.Identifier, FileId: thumbnailFile.FileId},
-		evt.ReplyTo)
+	return thumbnailFile.FileId, nil
 }
