e399841f5e4981d72d40b1a96bbf4b49e4cd850d
yongj.zhuang <yongj.zhuang@outlook.com>
2024-02-24 20:50:38 +0800

    Increase thumbnail size to *512

--- internal/hammer/video.go
+++ internal/hammer/video.go
@@ -8,7 +8,7 @@ import (
 )
 
 func ExtractFirstFrame(rail miso.Rail, url string, output string) error {
-	cmd := exec.Command("ffmpeg", "-i", url, "-t", "1", "-frames:v", "1", "-vf", "scale=256:-2", output)
+	cmd := exec.Command("ffmpeg", "-i", url, "-t", "1", "-frames:v", "1", "-vf", "scale=512:-2", output)
 	stdout, err := cmd.Output()
 	if err != nil {
 		return fmt.Errorf("failed to call ffmpeg for url: %v, target output: %v, %w", url, output, err)
